9b0f537a6547ef3885cb4c9bf1072a32ca7bde1a
LinT0T <737138090@qq.com>
2021-05-24 20:56:13 +0800

    分包

--- app/src/main/java/com/greenhand/cooperativework/view/activity/MainActivity.kt
+++ app/src/main/java/com/greenhand/cooperativework/view/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.greenhand.cooperativework
+package com.greenhand.cooperativework.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.greenhand.cooperativework.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
