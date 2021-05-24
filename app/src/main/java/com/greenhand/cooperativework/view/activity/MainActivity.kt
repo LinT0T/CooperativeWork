4bf186d30b77a114bd1b0005d3c7554c3606cbec
zz010625 <1140143252@qq.com>
2021-05-25 00:07:43 +0800

    实现BottomNavigationView底部导航栏

--- app/src/main/java/com/greenhand/cooperativework/view/activity/MainActivity.kt
+++ app/src/main/java/com/greenhand/cooperativework/view/activity/MainActivity.kt
@@ -3,11 +3,15 @@ package com.greenhand.cooperativework.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.greenhand.cooperativework.R
 import com.greenhand.cooperativework.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var mBottomNavigationView: BottomNavigationView
+    private lateinit var mNavHostFragment:NavHostFragment
     private val mViewModel by lazy {
         ViewModelProvider(this).get(MainViewModel::class.java)
     }
@@ -15,5 +19,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
     }
+    private fun init(){
+        mBottomNavigationView = findViewById(R.id.nav_bottom_view)
+        mNavHostFragment = supportFragmentManager.findFragmentById(R.id.nav_home_fragment) as NavHostFragment
+        mBottomNavigationView.setupWithNavController(mNavHostFragment.navController) }
 }
