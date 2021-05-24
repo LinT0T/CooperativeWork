fa49b91fdfcfa33522ce596337ae9dc7c1d2c28b
985892345 <62180145+985892345@users.noreply.github.com>
2021-05-24 21:15:50 +0800

    分包

--- app/src/main/java/com/greenhand/cooperativework/view/activity/MainActivity.kt
+++ app/src/main/java/com/greenhand/cooperativework/view/activity/MainActivity.kt
@@ -2,9 +2,16 @@ package com.greenhand.cooperativework.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.greenhand.cooperativework.R
+import com.greenhand.cooperativework.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val mViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/greenhand/cooperativework/viewmodel/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.greenhand.cooperativework.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+/**
+ *@author 985892345
+ *@email 2767465918@qq.com
+ *@data 2021/5/24
+ *@description
+ */
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+}
