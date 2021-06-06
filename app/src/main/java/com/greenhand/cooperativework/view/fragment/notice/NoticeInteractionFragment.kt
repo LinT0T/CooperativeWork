0e54d54d546dd8c479b0f2370ccec116253b5b6e
Shuhuan Xi <shuhuanx@yeah.net>
2021-06-06 18:10:04 +0800

    feat (NoticeInteractionFragment相关): Item的布局

--- app/src/main/java/com/greenhand/cooperativework/view/fragment/notice/NoticeInteractionFragment.kt
+++ app/src/main/java/com/greenhand/cooperativework/view/fragment/notice/NoticeInteractionFragment.kt
@@ -1,7 +1,14 @@
 package com.greenhand.cooperativework.view.fragment.notice
 
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.greenhand.cooperativework.bean.NoticeInteractionBean
+import com.greenhand.cooperativework.utils.toast
 
 class NoticeInteractionFragment: Fragment() {
-
+    inner class EventHandle(data:NoticeInteractionBean.Item.Data){
+        fun onClick(view: View){
+            "缺少API".toast()
+        }
+    }
 }
--- app/src/main/java/com/greenhand/cooperativework/viewmodel/fragment/NoticeInteractionViewModel.kt
+++ app/src/main/java/com/greenhand/cooperativework/viewmodel/fragment/NoticeInteractionViewModel.kt
@@ -3,4 +3,5 @@ package com.greenhand.cooperativework.viewmodel.fragment
 import androidx.lifecycle.ViewModel
 
 class NoticeInteractionViewModel:ViewModel() {
+
 }
